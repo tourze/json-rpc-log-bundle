84c2acf713563bdc57e410d2ab1cd9b3c3c3d51e
tourze-ci <you@example.com>
2025-04-15 02:28:56 +0000

    ci: 添加 PHPUnit 测试工作流

---
+++ src/Attribute/Log.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tourze\JsonRPCLogBundle\Attribute;
+
+/**
+ * 如果方法做了这个标记，则会将请求日志存入数据库
+ *
+ * 建议只在写操作、耗时统计这类接口上加这个注解
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+class Log
+{
+    public function __construct(
+        public readonly bool $request = true,
+        public readonly bool $response = true,
+    ) {
+    }
+}
---
+++ src/JsonRPCLogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tourze\JsonRPCLogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JsonRPCLogBundle extends Bundle
+{
+}
